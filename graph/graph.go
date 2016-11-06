9249037afcaafdb562c74f162555b396eaeea0cd
Martin Bruse <zond@google.com>
2016-11-06 01:05:58 +0100

    Made the graph type JSON renderable.

--- graph/graph.go
+++ graph/graph.go
@@ -9,17 +9,17 @@ import (
 
 func New() *Graph {
 	return &Graph{
-		nodes: make(map[common.Province]*node),
+		Nodes: make(map[common.Province]*Node),
 	}
 }
 
 type Graph struct {
-	nodes map[common.Province]*node
+	Nodes map[common.Province]*Node
 }
 
 func (self *Graph) String() string {
 	buf := new(bytes.Buffer)
-	for _, n := range self.nodes {
+	for _, n := range self.Nodes {
 		fmt.Fprintf(buf, "%v", n)
 	}
 	return string(buf.Bytes())
@@ -27,8 +27,8 @@ func (self *Graph) String() string {
 
 func (self *Graph) Has(n common.Province) (result bool) {
 	p, c := n.Split()
-	if node, ok := self.nodes[p]; ok {
-		if _, ok := node.subs[c]; ok {
+	if node, ok := self.Nodes[p]; ok {
+		if _, ok := node.Subs[c]; ok {
 			result = true
 		}
 	}
@@ -38,9 +38,9 @@ func (self *Graph) Has(n common.Province) (result bool) {
 func (self *Graph) AllFlags(n common.Province) (result map[common.Flag]bool) {
 	result = map[common.Flag]bool{}
 	p, _ := n.Split()
-	if node, ok := self.nodes[p]; ok {
-		for _, sub := range node.subs {
-			for flag, _ := range sub.flags {
+	if node, ok := self.Nodes[p]; ok {
+		for _, sub := range node.Subs {
+			for flag, _ := range sub.Flags {
 				result[flag] = true
 			}
 		}
@@ -50,24 +50,24 @@ func (self *Graph) AllFlags(n common.Province) (result map[common.Flag]bool) {
 
 func (self *Graph) Flags(n common.Province) (result map[common.Flag]bool) {
 	p, c := n.Split()
-	if node, ok := self.nodes[p]; ok {
-		if sub, ok := node.subs[c]; ok {
-			result = sub.flags
+	if node, ok := self.Nodes[p]; ok {
+		if sub, ok := node.Subs[c]; ok {
+			result = sub.Flags
 		}
 	}
 	return
 }
 
 func (self *Graph) SC(n common.Province) (result *common.Nation) {
-	if node, ok := self.nodes[n.Super()]; ok {
-		result = node.sc
+	if node, ok := self.Nodes[n.Super()]; ok {
+		result = node.SC
 	}
 	return
 }
 
 func (self *Graph) SCs(n common.Nation) (result []common.Province) {
-	for name, node := range self.nodes {
-		if node.sc != nil && *node.sc == n {
+	for name, node := range self.Nodes {
+		if node.SC != nil && *node.SC == n {
 			result = append(result, name)
 		}
 	}
@@ -77,16 +77,16 @@ func (self *Graph) SCs(n common.Nation) (result []common.Province) {
 func (self *Graph) Edges(n common.Province) (result map[common.Province]map[common.Flag]bool) {
 	result = map[common.Province]map[common.Flag]bool{}
 	for p, edge := range self.edges(n) {
-		result[p] = edge.flags
+		result[p] = edge.Flags
 	}
 	return
 }
 
 func (self *Graph) edges(n common.Province) (result map[common.Province]*edge) {
 	p, c := n.Split()
-	if node, ok := self.nodes[p]; ok {
-		if sub, ok := node.subs[c]; ok {
-			result = sub.edges
+	if node, ok := self.Nodes[p]; ok {
+		if sub, ok := node.Subs[c]; ok {
+			result = sub.Edges
 		}
 	}
 	return
@@ -103,7 +103,7 @@ func (self *Graph) pathHelper(dst common.Province, queue []pathStep, filter comm
 		seen[step.pos] = true
 		for name, edge := range self.edges(step.pos) {
 			if !seen[name] {
-				if filter == nil || filter(name, edge.flags, edge.sub.flags, edge.sub.node.sc) {
+				if filter == nil || filter(name, edge.Flags, edge.sub.Flags, edge.sub.node.SC) {
 					thisPath := append(append([]common.Province{}, step.path...), name)
 					if name == dst {
 						return thisPath
@@ -133,99 +133,102 @@ func (self *Graph) Path(src, dst common.Province, filter common.PathFilter) []co
 }
 
 func (self *Graph) Coasts(prov common.Province) (result []common.Province) {
-	if node, ok := self.nodes[prov.Super()]; ok {
-		for _, sub := range node.subs {
+	if node, ok := self.Nodes[prov.Super()]; ok {
+		for _, sub := range node.Subs {
 			result = append(result, sub.getName())
 		}
 	}
 	return
 }
 
-func (self *Graph) Prov(n common.Province) *subNode {
+func (self *Graph) Prov(n common.Province) *SubNode {
 	p, c := n.Split()
-	if self.nodes[p] == nil {
-		self.nodes[p] = &node{
-			name:  p,
-			subs:  make(map[common.Province]*subNode),
+	if self.Nodes[p] == nil {
+		self.Nodes[p] = &Node{
+			Name:  p,
+			Subs:  make(map[common.Province]*SubNode),
 			graph: self,
 		}
 	}
-	return self.nodes[p].sub(c)
+	return self.Nodes[p].sub(c)
 }
 
 func (self *Graph) Provinces() (result []common.Province) {
-	for _, node := range self.nodes {
-		for _, sub := range node.subs {
+	for _, node := range self.Nodes {
+		for _, sub := range node.Subs {
 			result = append(result, sub.getName())
 		}
 	}
 	return
 }
 
-type node struct {
-	name  common.Province
-	subs  map[common.Province]*subNode
-	sc    *common.Nation
+type Node struct {
+	Name common.Province
+	Subs map[common.Province]*SubNode
+	SC   *common.Nation
+
 	graph *Graph
 }
 
-func (self *node) String() string {
+func (self *Node) String() string {
 	buf := new(bytes.Buffer)
-	fmt.Fprintf(buf, "%v", self.name)
-	if self.sc != nil {
-		fmt.Fprintf(buf, " %v", *self.sc)
+	fmt.Fprintf(buf, "%v", self.Name)
+	if self.SC != nil {
+		fmt.Fprintf(buf, " %v", *self.SC)
 	}
-	if sub, ok := self.subs[""]; ok {
+	if sub, ok := self.Subs[""]; ok {
 		fmt.Fprintf(buf, " %v\n", sub)
 	}
-	for _, s := range self.subs {
-		if s.name != "" {
+	for _, s := range self.Subs {
+		if s.Name != "" {
 			fmt.Fprintf(buf, "  %v\n", s)
 		}
 	}
 	return string(buf.Bytes())
 }
 
-func (self *node) sub(n common.Province) *subNode {
-	if self.subs[n] == nil {
-		self.subs[n] = &subNode{
-			name:  n,
-			edges: make(map[common.Province]*edge),
+func (self *Node) sub(n common.Province) *SubNode {
+	if self.Subs[n] == nil {
+		self.Subs[n] = &SubNode{
+			Name:  n,
+			Edges: make(map[common.Province]*edge),
 			node:  self,
-			flags: make(map[common.Flag]bool),
+			Flags: make(map[common.Flag]bool),
 		}
 	}
-	return self.subs[n]
+	return self.Subs[n]
 }
 
 type edge struct {
-	sub   *subNode
-	flags map[common.Flag]bool
+	Flags map[common.Flag]bool
+
+	sub *SubNode
 }
 
-type subNode struct {
-	name  common.Province
-	edges map[common.Province]*edge
-	node  *node
-	flags map[common.Flag]bool
+type SubNode struct {
+	Name  common.Province
+	Edges map[common.Province]*edge
+	Flags map[common.Flag]bool
+
+	node *Node
 }
 
-func (self *subNode) String() string {
+func (self *SubNode) String() string {
 	buf := new(bytes.Buffer)
-	if self.name != "" {
-		fmt.Fprintf(buf, "%v ", self.name)
+	if self.Name != "" {
+		fmt.Fprintf(buf, "%v ", self.Name)
 	}
-	flags := make([]common.Flag, 0, len(self.flags))
-	for flag, _ := range self.flags {
+	flags := make([]common.Flag, 0, len(self.Flags))
+	for flag, _ := range self.Flags {
 		flags = append(flags, flag)
 	}
 	if len(flags) > 0 {
 		fmt.Fprintf(buf, "%v ", flags)
 	}
-	dests := make([]string, 0, len(self.edges))
-	for n, edge := range self.edges {
+	dests := make([]string, 0, len(self.Edges))
+	for n, edge := range self.Edges {
 		var flags []common.Flag
-		for f, _ := range edge.flags {
+		for f, _ := range edge.Flags {
 			flags = append(flags, f)
 		}
 		if len(flags) > 0 {
@@ -238,39 +241,39 @@ func (self *subNode) String() string {
 	return string(buf.Bytes())
 }
 
-func (self *subNode) getName() common.Province {
-	return self.node.name.Join(self.name)
+func (self *SubNode) getName() common.Province {
+	return self.node.Name.Join(self.Name)
 }
 
-func (self *subNode) Conn(n common.Province, flags ...common.Flag) *subNode {
+func (self *SubNode) Conn(n common.Province, flags ...common.Flag) *SubNode {
 	target := self.node.graph.Prov(n)
 	flagMap := make(map[common.Flag]bool)
 	for _, flag := range flags {
 		flagMap[flag] = true
 	}
-	self.edges[target.getName()] = &edge{
+	self.Edges[target.getName()] = &edge{
 		sub:   target,
-		flags: flagMap,
+		Flags: flagMap,
 	}
 	return self
 }
 
-func (self *subNode) SC(n common.Nation) *subNode {
-	self.node.sc = &n
+func (self *SubNode) SC(n common.Nation) *SubNode {
+	self.node.SC = &n
 	return self
 }
 
-func (self *subNode) Flag(flags ...common.Flag) *subNode {
+func (self *SubNode) Flag(flags ...common.Flag) *SubNode {
 	for _, flag := range flags {
-		self.flags[flag] = true
+		self.Flags[flag] = true
 	}
 	return self
 }
 
-func (self *subNode) Prov(n common.Province) *subNode {
+func (self *SubNode) Prov(n common.Province) *SubNode {
 	return self.node.graph.Prov(n)
 }
 
-func (self *subNode) Done() *Graph {
+func (self *SubNode) Done() *Graph {
 	return self.node.graph
 }
