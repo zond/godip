e9602f49a5123732af97e68fe69425b80ef8c293
Martin Bruse <zond@google.com>
2016-12-07 07:45:08 +0100

    Added a utility to Graph to get only super provinces, and only with a given SC availability.

--- common/common.go
+++ common/common.go
@@ -147,6 +147,7 @@ type Graph interface {
 	Edges(src Province) map[Province]map[Flag]bool
 	SCs(Nation) []Province
 	Provinces() []Province
+	SuperProvinces(sc bool) []Province
 }
 
 type Orders []Order
