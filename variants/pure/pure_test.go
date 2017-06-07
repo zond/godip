0ef820dee4dcbab501fb7331aff40d30252c8c1f
tttppp <tttppp@users.noreply.github.com>
2017-06-07 16:35:06 +0100

    Fix supply center creation.

--- variants/pure/pure_test.go
+++ variants/pure/pure_test.go
@@ -3,7 +3,6 @@ package pure
 import (
 	"bytes"
 	"encoding/xml"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -31,7 +30,10 @@ func createFile(name string, bin_data []byte) {
 }
 
 func openFile(name string) *os.File {
-	file, _ := os.Open(createTempPath(name))
+	file, err := os.OpenFile(createTempPath(name), os.O_WRONLY | os.O_CREATE | os.O_TRUNC, 0644)
+	if err != nil {
+		panic(err)
+	}
 	return file
 }
 
@@ -62,6 +64,27 @@ func findAttr(attrs []xml.Attr, name string) *xml.Attr {
 	return nil
 }
 
+func setAttr(attrs []xml.Attr, name string, value string) {
+	for i, attr := range attrs {
+		if attr.Name.Local == name {
+			attrs[i].Value = value
+			return
+		}
+	}
+	attrs = append(attrs, xml.Attr{xml.Name{"http://www.w3.org/2000/svg", name}, value})
+}
+
+func removeAttr(attrs []xml.Attr, name string) {
+	for i, attr := range attrs {
+		if attr.Name.Local == name {
+			copy(attrs[i:], attrs[i+1:])
+			attrs[len(attrs)-1] = xml.Attr{xml.Name{"",""}, ""}
+			attrs = attrs[:len(attrs)-1]
+			return
+		}
+	}
+}
+
 // Create svg files which can be inspected manually to check the binary map data is correct.
 func TestDrawMaps(t *testing.T) {
 	variant := PureVariant
@@ -76,7 +99,7 @@ func TestDrawMaps(t *testing.T) {
 	// Fill each SC red and output it
 	xmlFile := bytes.NewReader(b)
 	decoder := xml.NewDecoder(xmlFile)
-	encoder := xml.NewEncoder(openFile("deleteme.svg"))
+	encoder := xml.NewEncoder(openFile("supply_centers.svg"))
 	for {
 		token, _ := decoder.Token()
 		if token == nil {
@@ -87,19 +110,28 @@ func TestDrawMaps(t *testing.T) {
 			var idAttr = findAttr(startElement.Attr, "id")
 			if idAttr != nil {
 				id := idAttr.Value
-				if variantContains(variant, id) {
+				// Ensure the provinces layer is visible.
+				if id == "provinces" {
+					removeAttr(startElement.Attr, "style")
+				} else if variantContains(variant, id) {
+					// Colour each supply center province red.
 					var styleAttr = findAttr(startElement.Attr, "style")
 					if styleAttr != nil {
 						var style = styleAttr.Value
 						var re = regexp.MustCompile(`fill:[^;]+`)
 						newStyle := re.ReplaceAllString(style, `fill:#ff0000`)
-						fmt.Println(id + " " + style + " " + newStyle)
 						styleAttr.Value = newStyle
+						setAttr(startElement.Attr, "style", newStyle)
 					}
 				}
 			}
+			// Remove the duplicate xmlns attribute from the root element.
+			// See https://github.com/golang/go/issues/13400 for the ongoing issues with this.
+			if startElement.Name.Local == "svg" {
+				removeAttr(startElement.Attr, "xmlns")
+			}
 		}
-		// TODO: Why doesn't this do anything?
 		encoder.EncodeToken(token)
 	}
+	encoder.Flush()
 }
