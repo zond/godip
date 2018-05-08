949b959ba6d75ace5ccb80d07e89927e8ae372a3
Martin Bruse <zondolfin@gmail.com>
2018-05-08 08:30:13 +0200

    Fixed the app engine deployment.

---
+++ gae/common.go
@@ -0,0 +1,61 @@
+package gae
+
+import (
+	"github.com/zond/godip"
+	"github.com/zond/godip/state"
+	"github.com/zond/godip/variants/classical"
+	"github.com/zond/godip/variants/common"
+)
+
+type Phase struct {
+	Season        godip.Season
+	Year          int
+	Type          godip.PhaseType
+	Units         map[godip.Province]godip.Unit
+	Orders        map[godip.Nation]map[godip.Province][]string
+	SupplyCenters map[godip.Province]godip.Nation
+	Dislodgeds    map[godip.Province]godip.Unit
+	Dislodgers    map[godip.Province]godip.Province
+	Bounces       map[godip.Province]map[godip.Province]bool
+	Resolutions   map[godip.Province]string
+}
+
+func NewPhase(state *state.State) *Phase {
+	currentPhase := state.Phase()
+	p := &Phase{
+		Orders:      map[godip.Nation]map[godip.Province][]string{},
+		Resolutions: map[godip.Province]string{},
+		Season:      currentPhase.Season(),
+		Year:        currentPhase.Year(),
+		Type:        currentPhase.Type(),
+	}
+	var resolutions map[godip.Province]error
+	p.Units, p.SupplyCenters, p.Dislodgeds, p.Dislodgers, p.Bounces, resolutions = state.Dump()
+	for prov, err := range resolutions {
+		if err == nil {
+			p.Resolutions[prov] = "OK"
+		} else {
+			p.Resolutions[prov] = err.Error()
+		}
+	}
+	return p
+}
+
+func (self *Phase) State(variant common.Variant) (*state.State, error) {
+	parsedOrders, err := classical.Parser.ParseAll(self.Orders)
+	if err != nil {
+		return nil, err
+	}
+	return classical.Blank(variant.Phase(
+		self.Year,
+		self.Season,
+		self.Type,
+	)).Load(
+		self.Units,
+		self.SupplyCenters,
+		self.Dislodgeds,
+		self.Dislodgers,
+		self.Bounces,
+		parsedOrders,
+	), nil
+}
---
+++ gae/router.go
@@ -0,0 +1,91 @@
+package gae
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/zond/godip/variants"
+)
+
+func corsHeaders(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+}
+
+func preflight(w http.ResponseWriter, r *http.Request) {
+	corsHeaders(w)
+}
+
+func resolve(w http.ResponseWriter, r *http.Request) {
+	corsHeaders(w)
+	variantName := mux.Vars(r)["variant"]
+	variant, found := variants.Variants[variantName]
+	if !found {
+		http.Error(w, fmt.Sprintf("Variant %q not found", variantName), 404)
+		return
+	}
+	p := &Phase{}
+	if err := json.NewDecoder(r.Body).Decode(p); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	state, err := p.State(variant)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	if err = state.Next(); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	// Load the new godip phase from the state
+	nextPhase := NewPhase(state)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	if err = json.NewEncoder(w).Encode(nextPhase); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+}
+
+func start(w http.ResponseWriter, r *http.Request) {
+	corsHeaders(w)
+	variantName := mux.Vars(r)["variant"]
+	variant, found := variants.Variants[variantName]
+	if !found {
+		http.Error(w, fmt.Sprintf("Variant %q not found", variantName), 404)
+		return
+	}
+	state, err := variant.Start()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	phase := NewPhase(state)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	if err = json.NewEncoder(w).Encode(phase); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+}
+
+func listVariants(w http.ResponseWriter, r *http.Request) {
+	corsHeaders(w)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	if err := json.NewEncoder(w).Encode(variants.Variants); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+}
+
+func init() {
+	r := mux.NewRouter()
+	r.Methods("OPTIONS").HandlerFunc(preflight)
+	variants := r.Path("/{variant}").Subrouter()
+	variants.Methods("POST").HandlerFunc(resolve)
+	variants.Methods("GET").HandlerFunc(start)
+	r.Path("/").HandlerFunc(listVariants)
+	http.Handle("/", r)
+}
