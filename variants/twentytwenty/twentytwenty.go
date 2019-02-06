daa5bb5ca71bef0876a5527a3ca72a4b35287d5f
tttppp <tttppp@users.noreply.github.com>
2019-02-06 19:18:42 +0000

    Minor fix to rendering of Dominican Republic.
    Also move Gulf of Mannar slightly.

--- variants/twentytwenty/twentytwenty.go
+++ variants/twentytwenty/twentytwenty.go
@@ -99,7 +99,7 @@ var TwentyTwentyVariant = common.Variant{
 	SVGMap: func() ([]byte, error) {
 		return Asset("svg/twentytwentymap.svg")
 	},
-	SVGVersion: "3",
+	SVGVersion: "4",
 	SVGUnits: map[godip.UnitType]func() ([]byte, error){
 		godip.Army: func() ([]byte, error) {
 			return classical.Asset("svg/army.svg")
