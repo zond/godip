75b3d474684c1e204a3cdf7b35c17bb8820432de
tttppp <tttppp@users.noreply.github.com>
2020-04-13 13:47:02 +0100

    Update bindata and svg versions for other maps.

--- variants/empiresandcoalitions/empiresandcoalitions.go
+++ variants/empiresandcoalitions/empiresandcoalitions.go
@@ -37,7 +37,7 @@ var EmpiresAndCoalitionsVariant = common.Variant{
 	SVGMap: func() ([]byte, error) {
 		return Asset("svg/empiresandcoalitionsmap.svg")
 	},
-	SVGVersion: "5",
+	SVGVersion: "6",
 	SVGUnits: map[godip.UnitType]func() ([]byte, error){
 		godip.Army: func() ([]byte, error) {
 			return classical.Asset("svg/army.svg")
