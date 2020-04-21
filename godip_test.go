d5011f95841b25c7af84acfd85600cea1e50dba8
Martin Bruse <zondolfin@gmail.com>
2020-04-21 10:24:58 +0200

    Made it possible to compare godip.Nations properly.

--- godip_test.go
+++ godip_test.go
@@ -22,3 +22,45 @@ func TestNationSorting(t *testing.T) {
 		t.Errorf("Wanted Russia, got %v", nations[2])
 	}
 }
+
+func TestNationEqual(t *testing.T) {
+	n := Nations{
+		Austria,
+		Russia,
+		England,
+	}
+	o := Nations{
+		Austria,
+		Russia,
+		England,
+	}
+	if !n.Equal(o) {
+		t.Errorf("Wanted equal, wasn't: %+v, %+v", n, o)
+	}
+	o = Nations{
+		Russia,
+		Austria,
+		England,
+	}
+	if !n.Equal(o) {
+		t.Errorf("Wanted equal, wasn't: %+v, %+v", n, o)
+	}
+	if n[0] != Austria || n[1] != Russia || n[2] != England || o[0] != Russia || o[1] != Austria || o[2] != England {
+		t.Errorf("Changed order during equal, not OK")
+	}
+	o = Nations{
+		Austria,
+		Turkey,
+		England,
+	}
+	if n.Equal(o) {
+		t.Errorf("Wanted inequal, was: %+v, %+v", n, o)
+	}
+	o = Nations{
+		Austria,
+		Russia,
+	}
+	if n.Equal(o) {
+		t.Errorf("Wanted inequal, was: %+v, %+v", n, o)
+	}
+}
