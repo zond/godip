9d0d136b97ed9ed90dd16493f188c08879a630f4
Martin Bruse <zondolfin@gmail.com>
2020-06-01 11:22:54 +0200

    Added long names to chaos.

--- variants/chaos/chaos.go
+++ variants/chaos/chaos.go
@@ -322,10 +322,11 @@ var ChaosVariant = common.Variant{
 			return classical.Asset("svg/fleet.svg")
 		},
 	},
-	CreatedBy:   "Unknown",
-	Version:     "",
-	Description: "Standard map. 34 players. 1 unit each. Chaos.",
-	SoloSCCount: func(*state.State) int { return 18 },
+	ProvinceLongNames: classical.ClassicalVariant.ProvinceLongNames,
+	CreatedBy:         "Unknown",
+	Version:           "",
+	Description:       "Standard map. 34 players. 1 unit each. Chaos.",
+	SoloSCCount:       func(*state.State) int { return 18 },
 	Rules: `First to 18 Supply Centers (SC) is the winner. 
 	Units may be built at any owned SC. 
 	The game starts with an adjustment phase where it is decided what unit to build. In this phase, all SC without orders build an army (to mitigate absent players).`,
