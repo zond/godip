925ddad1e4f6bcf6106e93c734d38fd30af97a64
Joren <7293081+JorenC@users.noreply.github.com>
2021-12-28 20:28:39 +0100

    First iteration Done

---
+++ variants/generator/unconstitutional.go
@@ -0,0 +1,103 @@
+package unconstitutional
+
+import (
+	"github.com/zond/godip"
+	"github.com/zond/godip/graph"
+	"github.com/zond/godip/state"
+	"github.com/zond/godip/variants/classical"
+	"github.com/zond/godip/variants/common"
+)
+
+const (
+	Pennsylvania  godip.Nation = "Pennsylvania"
+	SouthCarolina godip.Nation = "SouthCarolina"
+)
+
+var Nations = []godip.Nation{Pennsylvania, SouthCarolina}
+
+var UnconstitutionalVariant = common.Variant{
+	Name:              "Unconstitutional",
+	Graph:             func() godip.Graph { return UnconstitutionalGraph() },
+	Start:             UnconstitutionalStart,
+	Blank:             UnconstitutionalBlank,
+	Phase:             classical.NewPhase,
+	Parser:            classical.Parser,
+	Nations:           Nations,
+	PhaseTypes:        classical.PhaseTypes,
+	Seasons:           classical.Seasons,
+	UnitTypes:         classical.UnitTypes,
+	SoloWinner:        common.SCCountWinner(2),
+	SoloSCCount:       func(*state.State) int { return 2 },
+	ProvinceLongNames: provinceLongNames,
+	SVGMap: func() ([]byte, error) {
+		return Asset("svg/unconstitutionalmap.svg")
+	},
+	SVGVersion: "1",
+	SVGUnits: map[godip.UnitType]func() ([]byte, error){
+		godip.Army: func() ([]byte, error) {
+			return classical.Asset("svg/army.svg")
+		},
+		godip.Fleet: func() ([]byte, error) {
+			return classical.Asset("svg/fleet.svg")
+		},
+	},
+	CreatedBy:   "",
+	Version:     "",
+	Description: "",
+	Rules:       "",
+}
+
+func UnconstitutionalBlank(phase godip.Phase) *state.State {
+	return state.New(UnconstitutionalGraph(), phase, classical.BackupRule, nil, nil)
+}
+
+func UnconstitutionalStart() (result *state.State, err error) {
+	startPhase := classical.NewPhase(1805, godip.Spring, godip.Movement)
+	result = UnconstitutionalBlank(startPhase)
+	if err = result.SetUnits(map[godip.Province]godip.Unit{
+		"cho": godip.Unit{godip.Army, Pennsylvania},
+		"sai": godip.Unit{godip.Army, SouthCarolina},
+	}); err != nil {
+		return
+	}
+	result.SetSupplyCenters(map[godip.Province]godip.Nation{
+		"cho": Pennsylvania,
+		"sai": SouthCarolina,
+	})
+	return
+}
+
+func UnconstitutionalGraph() *graph.Graph {
+	return graph.New().
+		// Caddo
+		Prov("cad").Conn("gul", godip.Land).Conn("qua", godip.Land).Flag(godip.Land).
+		// Choctaw
+		Prov("cho").Conn("gul", godip.Land).Conn("atl", godip.Land).Conn("sai", godip.Land).Conn("qua", godip.Land).Flag(godip.Land).SC(Pennsylvania).
+		// Missouri
+		Prov("mis").Conn("osa", godip.Land).Conn("sai", godip.Land).Conn("ill", godip.Land).Conn("atl", godip.Land).Flag(godip.Land).
+		// Osage
+		Prov("osa").Conn("qua", godip.Land).Conn("sai", godip.Land).Conn("mis", godip.Land).Flag(godip.Land).
+		// Illiniwek
+		Prov("ill").Conn("sai", godip.Land).Conn("atl", godip.Land).Conn("mis", godip.Land).Flag(godip.Land).
+		// Saint Louis
+		Prov("sai").Conn("atl", godip.Land).Conn("ill", godip.Land).Conn("mis", godip.Land).Conn("osa", godip.Land).Conn("qua", godip.Land).Conn("cho", godip.Land).Flag(godip.Land).SC(SouthCarolina).
+		// Gulf of Mexico
+		Prov("gul").Conn("atl", godip.Land).Conn("cho", godip.Land).Conn("qua", godip.Land).Conn("cad", godip.Land).Flag(godip.Land).
+		// Quapow
+		Prov("qua").Conn("cad", godip.Land).Conn("gul", godip.Land).Conn("cho", godip.Land).Conn("sai", godip.Land).Conn("osa", godip.Land).Flag(godip.Land).
+		// Atlantic Ocean
+		Prov("atl").Conn("mis", godip.Land).Conn("ill", godip.Land).Conn("sai", godip.Land).Conn("cho", godip.Land).Conn("gul", godip.Land).Flag(godip.Land).
+		Done()
+}
+
+var provinceLongNames = map[godip.Province]string{
+	"cad": "Caddo",
+	"cho": "Choctaw",
+	"mis": "Missouri",
+	"osa": "Osage",
+	"ill": "Illiniwek",
+	"sai": "Saint Louis",
+	"gul": "Gulf of Mexico",
+	"qua": "Quapow",
+	"atl": "Atlantic Ocean",
+}
