3e01775fd6a9a9e7b0b6b2be5c91a31c31a81444
Joren <7293081+JorenC@users.noreply.github.com>
2021-12-31 10:11:01 +0100

    iteration 4

--- variants/generator/unconstitutional.go
+++ variants/generator/unconstitutional.go
@@ -69,80 +69,101 @@ func UnconstitutionalStart() (result *state.State, err error) {
 
 func UnconstitutionalGraph() *graph.Graph {
 	return graph.New().
+		// Old Bahama Channel
+		Prov("old").Conn("tur", godip.Sea).Conn("bah", godip.Sea).Conn("flo", godip.Sea).Conn("gue", godip.Sea).Conn("gul", godip.Sea).Conn("win", godip.Sea).Conn("art", godip.Sea).Conn("cib", godip.Sea).Conn("sar", godip.Sea).Flag(godip.Sea).
+		// New York Bight
+		Prov("nyb").Conn("che", godip.Sea).Conn("sar", godip.Sea).Conn("gua", godip.Sea).Conn("lis", godip.Sea).Conn("loi", godip.Sea).Conn("del", godip.Sea).Flag(godip.Sea).
 		// Artibonite
 		Prov("art").Conn("azu", godip.Land).Conn("cib", godip.Coast...).Conn("old", godip.Sea).Conn("win", godip.Sea).Conn("por", godip.Coast...).Flag(godip.Coast...).
+		// Chesapeake Bay
+		Prov("che").Conn("out", godip.Sea).Conn("sar", godip.Sea).Conn("nyb", godip.Sea).Conn("del", godip.Sea).Flag(godip.Sea).
 		// New Orleans
-		Prov("new").Conn("gue", godip.Sea).Conn("wes", godip.Coast...).Conn("qua", godip.Land).Flag(godip.Coast...).
+		Prov("neo").Conn("gue", godip.Sea).Conn("wes", godip.Coast...).Conn("qua", godip.Land).Flag(godip.Coast...).
 		// Quapow
-		Prov("qua").Conn("new", godip.Land).Conn("wes", godip.Land).Conn("cho", godip.Land).Conn("sal", godip.Land).Conn("osa", godip.Land).Flag(godip.Land).
+		Prov("qua").Conn("neo", godip.Land).Conn("wes", godip.Land).Conn("cho", godip.Land).Conn("sal", godip.Land).Conn("osa", godip.Land).Flag(godip.Land).
+		// Atlantic Ocean
+		Prov("atl").Conn("gua", godip.Sea).Conn("sar", godip.Sea).Conn("sad", godip.Sea).Conn("gul", godip.Sea).Flag(godip.Sea).
 		// Windward Passage
-		Prov("win").Conn("por", godip.Sea).Conn("art", godip.Sea).Conn("old", godip.Sea).Conn("gof", godip.Sea).Flag(godip.Sea).
+		Prov("win").Conn("por", godip.Sea).Conn("art", godip.Sea).Conn("old", godip.Sea).Conn("gul", godip.Sea).Flag(godip.Sea).
 		// Choctaw
-		Prov("cho").Conn("cad", godip.Coast...).Conn("gua", godip.Sea).Conn("sal", godip.Coast...).Conn("qua", godip.Land).Conn("wes", godip.Land).Flag(godip.Coast...).SC(Pennsylvania).
-		// Illiniwek
-		Prov("ill").Conn("gua", godip.Sea).Conn("mis", godip.Coast...).Conn("sal", godip.Coast...).Flag(godip.Coast...).
+		Prov("cho").Conn("cad", godip.Land).Conn("sal", godip.Land).Conn("qua", godip.Land).Conn("wes", godip.Land).Flag(godip.Land).SC(Pennsylvania).
 		// Gulf of Maine
-		Prov("gua").Conn("mis", godip.Sea).Conn("ill", godip.Sea).Conn("sal", godip.Sea).Conn("cho", godip.Sea).Conn("cad", godip.Sea).Conn("flo", godip.Sea).Conn("bah", godip.Sea).Conn("sar", godip.Sea).Conn("atl", godip.Sea).Flag(godip.Sea).
+		Prov("gua").Conn("lis", godip.Sea).Conn("nyb", godip.Sea).Conn("sar", godip.Sea).Conn("atl", godip.Sea).Flag(godip.Sea).
 		// Cibao
 		Prov("cib").Conn("sar", godip.Sea).Conn("old", godip.Sea).Conn("art", godip.Coast...).Conn("azu", godip.Land).Conn("sad", godip.Coast...).Flag(godip.Coast...).
-		// Port au Prince
-		Prov("por").Conn("gof", godip.Sea).Conn("azu", godip.Coast...).Conn("art", godip.Coast...).Conn("win", godip.Sea).Flag(godip.Coast...).SC(godip.Neutral).
-		// Saint Domingue
-		Prov("sad").Conn("gof", godip.Sea).Conn("atl", godip.Sea).Conn("sar", godip.Sea).Conn("cib", godip.Coast...).Conn("azu", godip.Coast...).Flag(godip.Coast...).SC(godip.Neutral).
-		// Turks and Caicos
-		Prov("tur").Conn("old", godip.Sea).Conn("sar", godip.Sea).Conn("bah", godip.Sea).Flag(godip.Coast...).SC(godip.Neutral).
-		// West Florida
-		Prov("wes").Conn("new", godip.Coast...).Conn("gue", godip.Sea).Conn("flo", godip.Sea).Conn("cad", godip.Coast...).Conn("cho", godip.Land).Conn("qua", godip.Land).Flag(godip.Coast...).SC(godip.Neutral).
-		// Caddo
-		Prov("cad").Conn("cho", godip.Coast...).Conn("wes", godip.Coast...).Conn("flo", godip.Sea).Conn("gua", godip.Sea).Flag(godip.Coast...).
-		// Gulf of Gonave
-		Prov("gof").Conn("atl", godip.Sea).Conn("sad", godip.Sea).Conn("azu", godip.Sea).Conn("por", godip.Sea).Conn("win", godip.Sea).Conn("old", godip.Sea).Conn("gue", godip.Sea).Flag(godip.Sea).
 		// Missouri
-		Prov("mis").Conn("osa", godip.Land).Conn("sal", godip.Coast...).Conn("ill", godip.Coast...).Conn("gua", godip.Sea).Flag(godip.Coast...).
+		Prov("mis").Conn("osa", godip.Land).Conn("sal", godip.Land).Conn("ill", godip.Land).Flag(godip.Land).
+		// Port au Prince
+		Prov("por").Conn("gul", godip.Sea).Conn("azu", godip.Coast...).Conn("art", godip.Coast...).Conn("win", godip.Sea).Flag(godip.Coast...).SC(godip.Neutral).
+		// Saint Domingue
+		Prov("sad").Conn("gul", godip.Sea).Conn("atl", godip.Sea).Conn("sar", godip.Sea).Conn("cib", godip.Coast...).Conn("azu", godip.Coast...).Flag(godip.Coast...).SC(godip.Neutral).
+		// Delaware Bay
+		Prov("del").Conn("che", godip.Sea).Conn("nyb", godip.Sea).Flag(godip.Sea).
+		// West Florida
+		Prov("wes").Conn("neo", godip.Coast...).Conn("gue", godip.Sea).Conn("flo", godip.Sea).Conn("cad", godip.Coast...).Conn("cho", godip.Land).Conn("qua", godip.Land).Flag(godip.Coast...).SC(godip.Neutral).
+		// Caddo
+		Prov("cad").Conn("cho", godip.Land).Conn("wes", godip.Coast...).Conn("flo", godip.Sea).Flag(godip.Coast...).
+		// Gulf of Gonave
+		Prov("gul").Conn("atl", godip.Sea).Conn("sad", godip.Sea).Conn("azu", godip.Sea).Conn("por", godip.Sea).Conn("win", godip.Sea).Conn("old", godip.Sea).Conn("gue", godip.Sea).Flag(godip.Sea).
+		// Long Island Sound
+		Prov("lis").Conn("loi", godip.Sea).Conn("nyb", godip.Sea).Conn("gua", godip.Sea).Flag(godip.Sea).
 		// Osage
 		Prov("osa").Conn("qua", godip.Land).Conn("sal", godip.Land).Conn("mis", godip.Land).Flag(godip.Land).
 		// Florida Bight
-		Prov("flo").Conn("wes", godip.Sea).Conn("gue", godip.Sea).Conn("old", godip.Sea).Conn("bah", godip.Sea).Conn("gua", godip.Sea).Conn("cad", godip.Sea).Flag(godip.Sea).
+		Prov("flo").Conn("wes", godip.Sea).Conn("gue", godip.Sea).Conn("old", godip.Sea).Conn("bah", godip.Sea).Conn("cad", godip.Sea).Flag(godip.Sea).
 		// Azua
-		Prov("azu").Conn("gof", godip.Sea).Conn("sad", godip.Coast...).Conn("cib", godip.Land).Conn("art", godip.Land).Conn("por", godip.Coast...).Flag(godip.Coast...).
+		Prov("azu").Conn("art", godip.Land).Conn("por", godip.Coast...).Conn("gul", godip.Sea).Conn("sad", godip.Coast...).Conn("cib", godip.Land).Flag(godip.Coast...).
 		// Bahama Banks
-		Prov("bah").Conn("tur", godip.Sea).Conn("sar", godip.Sea).Conn("gua", godip.Sea).Conn("flo", godip.Sea).Conn("old", godip.Sea).Flag(godip.Sea).
+		Prov("bah").Conn("tur", godip.Sea).Conn("sar", godip.Sea).Conn("geo", godip.Sea).Conn("flo", godip.Sea).Conn("old", godip.Sea).Flag(godip.Sea).
 		// Saint Louis
-		Prov("sal").Conn("qua", godip.Land).Conn("cho", godip.Coast...).Conn("gua", godip.Sea).Conn("ill", godip.Coast...).Conn("mis", godip.Coast...).Conn("osa", godip.Land).Flag(godip.Coast...).SC(SouthCarolina).
-		// Gulf of Mexico
-		Prov("gue").Conn("gof", godip.Sea).Conn("old", godip.Sea).Conn("flo", godip.Sea).Conn("wes", godip.Sea).Conn("new", godip.Sea).Flag(godip.Sea).
-		// Old Bahama Channel
-		Prov("old").Conn("tur", godip.Sea).Conn("bah", godip.Sea).Conn("flo", godip.Sea).Conn("gue", godip.Sea).Conn("gof", godip.Sea).Conn("win", godip.Sea).Conn("art", godip.Sea).Conn("cib", godip.Sea).Conn("sar", godip.Sea).Flag(godip.Sea).
-		// Atlantic Ocean
-		Prov("atl").Conn("gua", godip.Sea).Conn("sar", godip.Sea).Conn("sad", godip.Sea).Conn("gof", godip.Sea).Flag(godip.Sea).
+		Prov("sal").Conn("qua", godip.Land).Conn("cho", godip.Land).Conn("ill", godip.Land).Conn("mis", godip.Land).Conn("osa", godip.Land).Flag(godip.Land).SC(SouthCarolina).
+		// Illiniwek
+		Prov("ill").Conn("mis", godip.Land).Conn("sal", godip.Land).Flag(godip.Land).
+		// Turks and Caicos
+		Prov("tur").Conn("old", godip.Sea).Conn("sar", godip.Sea).Conn("bah", godip.Sea).Flag(godip.Coast...).SC(godip.Neutral).
+		// Long Island
+		Prov("loi").Conn("lis", godip.Sea).Conn("nyb", godip.Sea).Flag(godip.Coast...).SC(godip.Neutral).
+		// Georgia Bight
+		Prov("geo").Conn("out", godip.Sea).Conn("bah", godip.Sea).Conn("sar", godip.Sea).Flag(godip.Sea).
+		// Outer Banks
+		Prov("out").Conn("geo", godip.Sea).Conn("sar", godip.Sea).Conn("che", godip.Sea).Flag(godip.Sea).
 		// Sargasso Sea
-		Prov("sar").Conn("tur", godip.Sea).Conn("old", godip.Sea).Conn("cib", godip.Sea).Conn("sad", godip.Sea).Conn("atl", godip.Sea).Conn("gua", godip.Sea).Conn("bah", godip.Sea).Flag(godip.Sea).
+		Prov("sar").Conn("cib", godip.Sea).Conn("sad", godip.Sea).Conn("atl", godip.Sea).Conn("gua", godip.Sea).Conn("nyb", godip.Sea).Conn("che", godip.Sea).Conn("out", godip.Sea).Conn("geo", godip.Sea).Conn("bah", godip.Sea).Conn("tur", godip.Sea).Conn("old", godip.Sea).Flag(godip.Sea).
+		// Gulf of Mexico
+		Prov("gue").Conn("gul", godip.Sea).Conn("old", godip.Sea).Conn("flo", godip.Sea).Conn("wes", godip.Sea).Conn("neo", godip.Sea).Flag(godip.Sea).
 		Done()
 }
 
 var provinceLongNames = map[godip.Province]string{
+	"old": "Old Bahama Channel",
+	"nyb": "New York Bight",
 	"art": "Artibonite",
-	"new": "New Orleans",
+	"che": "Chesapeake Bay",
+	"neo": "New Orleans",
 	"qua": "Quapow",
+	"atl": "Atlantic Ocean",
 	"win": "Windward Passage",
 	"cho": "Choctaw",
-	"ill": "Illiniwek",
 	"gua": "Gulf of Maine",
 	"cib": "Cibao",
+	"mis": "Missouri",
 	"por": "Port au Prince",
 	"sad": "Saint Domingue",
-	"tur": "Turks and Caicos",
+	"del": "Delaware Bay",
 	"wes": "West Florida",
 	"cad": "Caddo",
-	"gof": "Gulf of Gonave",
-	"mis": "Missouri",
+	"gul": "Gulf of Gonave",
+	"lis": "Long Island Sound",
 	"osa": "Osage",
 	"flo": "Florida Bight",
 	"azu": "Azua",
 	"bah": "Bahama Banks",
 	"sal": "Saint Louis",
-	"gue": "Gulf of Mexico",
-	"old": "Old Bahama Channel",
-	"atl": "Atlantic Ocean",
+	"ill": "Illiniwek",
+	"tur": "Turks and Caicos",
+	"loi": "Long Island",
+	"geo": "Georgia Bight",
+	"out": "Outer Banks",
 	"sar": "Sargasso Sea",
+	"gue": "Gulf of Mexico",
 }
