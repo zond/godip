bcae3b047d67973343ccdeb807ec0cc63a0758e7
Joren <7293081+JorenC@users.noreply.github.com>
2023-02-16 22:25:37 +0100

    Next Step

--- variants/generator/spiceislands.go
+++ variants/generator/spiceislands.go
@@ -26,8 +26,8 @@ var SpiceIslandsVariant = common.Variant{
 	PhaseTypes:        classical.PhaseTypes,
 	Seasons:           classical.Seasons,
 	UnitTypes:         classical.UnitTypes,
-	SoloWinner:        common.SCCountWinner(2),
-	SoloSCCount:       func(*state.State) int { return 2 },
+	SoloWinner:        common.SCCountWinner(6),
+	SoloSCCount:       func(*state.State) int { return 6 },
 	ProvinceLongNames: provinceLongNames,
 	SVGMap: func() ([]byte, error) {
 		return Asset("svg/spiceislandsmap.svg")
@@ -61,7 +61,6 @@ func SpiceIslandsStart() (result *state.State, err error) {
 		return
 	}
 	result.SetSupplyCenters(map[godip.Province]godip.Nation{
-		"lum": Brunei,
 		"weh": Majapahit,
 	})
 	return
@@ -69,26 +68,86 @@ func SpiceIslandsStart() (result *state.State, err error) {
 
 func SpiceIslandsGraph() *graph.Graph {
 	return graph.New().
+		// Halmahera
+		Prov("hal").Conn("gul", godip.Sea).Conn("mol", godip.Sea).Conn("big", godip.Sea).Flag(godip.Coast...).SC(godip.Neutral).
+		// Gulf of Tomini
+		Prov("gul").Conn("mol", godip.Sea).Conn("hal", godip.Sea).Conn("big", godip.Sea).Conn("big", godip.Sea).Conn("mih", godip.Sea).Conn("luw", godip.Sea).Conn("ban", godip.Sea).Flag(godip.Sea).
 		// Wehali
-		Prov("weh").Conn("big", godip.Sea).Conn("sto", godip.Sea).Conn("tim", godip.Sea).Conn("big", godip.Sea).Flag(godip.Coast...).SC(Majapahit).
-		// Lumajang
-		Prov("lum").Conn("big", godip.Sea).Conn("sto", godip.Sea).Conn("big", godip.Sea).Conn("tro", godip.Coast...).Flag(godip.Coast...).SC(Brunei).
-		// Trowulan
-		Prov("tro").Conn("big", godip.Sea).Conn("big", godip.Sea).Conn("lum", godip.Coast...).Conn("big", godip.Sea).Flag(godip.Coast...).SC(godip.Neutral).
-		// Big Ocean
-		Prov("big").Conn("sto", godip.Sea).Conn("sto", godip.Sea).Conn("lum", godip.Sea).Conn("tro", godip.Sea).Conn("tro", godip.Sea).Conn("tro", godip.Sea).Conn("lum", godip.Sea).Conn("sto", godip.Sea).Conn("weh", godip.Sea).Conn("weh", godip.Sea).Conn("tim", godip.Sea).Flag(godip.Sea).
-		// Southern Ocean
-		Prov("sto").Conn("tim", godip.Sea).Conn("weh", godip.Sea).Conn("big", godip.Sea).Conn("lum", godip.Sea).Conn("big", godip.Sea).Conn("big", godip.Sea).Flag(godip.Sea).
+		Prov("weh").Conn("sto", godip.Sea).Conn("tim", godip.Sea).Conn("ban", godip.Sea).Conn("big", godip.Sea).Flag(godip.Coast...).SC(Majapahit).
+		// Aceh
+		Prov("ace").Conn("wes", godip.Sea).Conn("ped", godip.Coast...).Conn("big", godip.Sea).Conn("big", godip.Sea).Conn("mig", godip.Coast...).Flag(godip.Coast...).
 		// Timor Sea
-		Prov("tim").Conn("big", godip.Sea).Conn("weh", godip.Sea).Conn("sto", godip.Sea).Flag(godip.Sea).
+		Prov("tim").Conn("big", godip.Sea).Conn("ser", godip.Sea).Conn("mol", godip.Sea).Conn("bur", godip.Sea).Conn("ban", godip.Sea).Conn("weh", godip.Sea).Conn("sto", godip.Sea).Flag(godip.Sea).
+		// Makassar
+		Prov("mak").Conn("ban", godip.Sea).Conn("but", godip.Coast...).Conn("luw", godip.Coast...).Conn("mih", godip.Coast...).Conn("big", godip.Sea).Flag(godip.Coast...).SC(godip.Neutral).
+		// Jambi
+		Prov("jam").Conn("ria", godip.Coast...).Conn("wes", godip.Sea).Conn("pal", godip.Coast...).Conn("big", godip.Sea).Flag(godip.Coast...).
+		// Big Ocean
+		Prov("big").Conn("mig", godip.Sea).Conn("ace", godip.Sea).Conn("ace", godip.Sea).Conn("ped", godip.Sea).Conn("ria", godip.Sea).Conn("ria", godip.Sea).Conn("jam", godip.Sea).Conn("pal", godip.Sea).Conn("pal", godip.Sea).Conn("mig", godip.Sea).Conn("paj", godip.Sea).Conn("jav", godip.Sea).Conn("tro", godip.Sea).Conn("tro", godip.Sea).Conn("lum", godip.Sea).Conn("sto", godip.Sea).Conn("weh", godip.Sea).Conn("ban", godip.Sea).Conn("mak", godip.Sea).Conn("mih", godip.Sea).Conn("mih", godip.Sea).Conn("gul", godip.Sea).Conn("gul", godip.Sea).Conn("hal", godip.Sea).Conn("mol", godip.Sea).Conn("ser", godip.Sea).Conn("tim", godip.Sea).Flag(godip.Sea).
+		// Riau
+		Prov("ria").Conn("ped", godip.Coast...).Conn("wes", godip.Sea).Conn("jam", godip.Coast...).Conn("big", godip.Sea).Conn("big", godip.Sea).Flag(godip.Coast...).SC(godip.Neutral).
+		// Seram
+		Prov("ser").Conn("tim", godip.Sea).Conn("big", godip.Sea).Conn("mol", godip.Sea).Flag(godip.Coast...).SC(godip.Neutral).
+		// Palembang
+		Prov("pal").Conn("wes", godip.Sea).Conn("mig", godip.Coast...).Conn("big", godip.Sea).Conn("big", godip.Sea).Conn("jam", godip.Coast...).Flag(godip.Coast...).SC(godip.Neutral).
+		// Lumajang
+		Prov("lum").Conn("big", godip.Sea).Conn("tro", godip.Coast...).Conn("jav", godip.Coast...).Conn("sto", godip.Sea).Flag(godip.Coast...).
+		// Western Ocean
+		Prov("wes").Conn("ace", godip.Sea).Conn("mig", godip.Sea).Conn("pal", godip.Sea).Conn("jam", godip.Sea).Conn("ria", godip.Sea).Conn("ped", godip.Sea).Flag(godip.Sea).
+		// Trowulan
+		Prov("tro").Conn("jav", godip.Coast...).Conn("lum", godip.Coast...).Conn("big", godip.Sea).Conn("big", godip.Sea).Flag(godip.Coast...).SC(godip.Neutral).
+		// Sunda
+		Prov("sun").Conn("paj", godip.Land).Conn("mig", godip.Land).Conn("jav", godip.Land).Flag(godip.Land).
+		// Buru
+		Prov("bur").Conn("tim", godip.Sea).Conn("mol", godip.Sea).Conn("ban", godip.Sea).Flag(godip.Coast...).SC(godip.Neutral).
+		// Moluccan Sea
+		Prov("mol").Conn("tim", godip.Sea).Conn("ser", godip.Sea).Conn("big", godip.Sea).Conn("hal", godip.Sea).Conn("gul", godip.Sea).Conn("ban", godip.Sea).Conn("bur", godip.Sea).Flag(godip.Sea).
+		// Javadvipa
+		Prov("jav").Conn("paj", godip.Coast...).Conn("sun", godip.Land).Conn("mig", godip.Coast...).Conn("sto", godip.Sea).Conn("lum", godip.Coast...).Conn("tro", godip.Coast...).Conn("big", godip.Sea).Flag(godip.Coast...).SC(godip.Neutral).
+		// Pajajaran
+		Prov("paj").Conn("jav", godip.Coast...).Conn("big", godip.Sea).Conn("mig", godip.Coast...).Conn("sun", godip.Land).Flag(godip.Coast...).SC(godip.Neutral).
+		// Southern Ocean
+		Prov("sto").Conn("tim", godip.Sea).Conn("weh", godip.Sea).Conn("big", godip.Sea).Conn("lum", godip.Sea).Conn("jav", godip.Sea).Conn("mig", godip.Sea).Flag(godip.Sea).
+		// Banda Sea
+		Prov("ban").Conn("big", godip.Sea).Conn("weh", godip.Sea).Conn("tim", godip.Sea).Conn("bur", godip.Sea).Conn("mol", godip.Sea).Conn("gul", godip.Sea).Conn("luw", godip.Sea).Conn("but", godip.Sea).Conn("mak", godip.Sea).Flag(godip.Sea).
+		// Minangkabau
+		Prov("mig").Conn("sto", godip.Sea).Conn("jav", godip.Coast...).Conn("sun", godip.Land).Conn("paj", godip.Coast...).Conn("big", godip.Sea).Conn("pal", godip.Coast...).Conn("wes", godip.Sea).Conn("ace", godip.Coast...).Conn("big", godip.Sea).Flag(godip.Coast...).
+		// Pedir
+		Prov("ped").Conn("ria", godip.Coast...).Conn("big", godip.Sea).Conn("ace", godip.Coast...).Conn("wes", godip.Sea).Flag(godip.Coast...).
+		// Minahassa
+		Prov("mih").Conn("big", godip.Sea).Conn("big", godip.Sea).Conn("mak", godip.Coast...).Conn("luw", godip.Coast...).Conn("gul", godip.Sea).Flag(godip.Coast...).SC(godip.Neutral).
+		// Luwuk
+		Prov("luw").Conn("but", godip.Coast...).Conn("ban", godip.Sea).Conn("gul", godip.Sea).Conn("mih", godip.Coast...).Conn("mak", godip.Coast...).Flag(godip.Coast...).
+		// Buton
+		Prov("but").Conn("luw", godip.Coast...).Conn("mak", godip.Coast...).Conn("ban", godip.Sea).Flag(godip.Coast...).
 		Done()
 }
 
 var provinceLongNames = map[godip.Province]string{
+	"hal": "Halmahera",
+	"gul": "Gulf of Tomini",
 	"weh": "Wehali",
-	"lum": "Lumajang",
-	"tro": "Trowulan",
-	"big": "Big Ocean",
-	"sto": "Southern Ocean",
+	"ace": "Aceh",
 	"tim": "Timor Sea",
+	"mak": "Makassar",
+	"jam": "Jambi",
+	"big": "Big Ocean",
+	"ria": "Riau",
+	"ser": "Seram",
+	"pal": "Palembang",
+	"lum": "Lumajang",
+	"wes": "Western Ocean",
+	"tro": "Trowulan",
+	"sun": "Sunda",
+	"bur": "Buru",
+	"mol": "Moluccan Sea",
+	"jav": "Javadvipa",
+	"paj": "Pajajaran",
+	"sto": "Southern Ocean",
+	"ban": "Banda Sea",
+	"mig": "Minangkabau",
+	"ped": "Pedir",
+	"mih": "Minahassa",
+	"luw": "Luwuk",
+	"but": "Buton",
 }
