c0ac70822c8cd0ac2a05897b51da1fc71794abb6
Joren <7293081+JorenC@users.noreply.github.com>
2023-03-08 20:51:42 +0100

    Add Build Anywhere

--- variants/beta/southofsahara/southofsahara.go
+++ variants/beta/southofsahara/southofsahara.go
@@ -38,10 +38,10 @@ var SouthofSaharaVariant = common.Variant{
 	SVGVersion: "1",
 	SVGUnits: map[godip.UnitType]func() ([]byte, error){
 		godip.Army: func() ([]byte, error) {
-			return classical.Asset("svg/army.svg")
+			return Asset("svg/army.svg")
 		},
 		godip.Fleet: func() ([]byte, error) {
-			return classical.Asset("svg/fleet.svg")
+			return Asset("svg/fleet.svg")
 		},
 	},
 	CreatedBy:   "",
