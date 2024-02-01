00a9cd2d59bf6416ae93beece95db11656ee6e05
TJ Brooks <tjbrooks@timothybrooks.org>
2024-02-01 14:02:30 -0600

    feature: ClassicalCrowded
    Initial Implementation of Classical - Crowded Variant

---
+++ variants/classicalcrowded/droidippy/droidippy_test.go
@@ -0,0 +1,18 @@
+package droidippy
+
+import (
+	"testing"
+
+	"github.com/zond/godip/variants/classical"
+
+	dip "github.com/zond/godip"
+	tst "github.com/zond/godip/variants/testing"
+)
+
+func init() {
+	dip.Debug = true
+}
+
+func TestDroidippyGames(t *testing.T) {
+	tst.TestGames(t, classical.ClassicalVariant)
+}
